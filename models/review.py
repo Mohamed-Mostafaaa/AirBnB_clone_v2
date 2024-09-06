0fd047217cec6e99c085e54cc8e959d4743b3af1
Mohamed-Mostafaaa <momostafa110022@gmail.com>
2024-09-06 21:36:18 +0300

    review

--- models/review.py
+++ models/review.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
+"""This is the review class"""
+import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 
-"""Review model that inherits from the BaseModel"""
 
-from models.base_model import BaseModel
-
-
-class Review(BaseModel):
-    """
-    Review Class
+class Review(BaseModel, Base):
+    """This is the class for Review
     Attributes:
-        place_id (str) -> Place.id (place being reviewed)
-        user_id (str) -> User.id ( Reviwer)
-        text (str) -> The review text
+        place_id: place id
+        user_id: user id
+        text: review description
     """
-    place_id = ""
-    user_id = ""
-    text = ""
+    __tablename__ = "reviews"
+
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    text = Column(String(1024), nullable=False)
