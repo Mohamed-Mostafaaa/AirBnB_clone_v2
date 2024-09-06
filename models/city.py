8849e60e4241ca3773f08123ae9de99a50316262
Mohamed-Mostafaaa <momostafa110022@gmail.com>
2024-09-06 21:35:46 +0300

    city

--- models/city.py
+++ models/city.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-"""
-City Module inheriting from the BaseModel
-"""
+"""This is the city class"""
+import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
-from models.base_model import BaseModel
 
-
-class City(BaseModel):
-    """City Class
+class City(BaseModel, Base):
+    """This is the class for City
     Attributes:
-        state_id (str) -> State.id
-        name (str) -> name of the city
+        state_id: The state id
+        name: input name
     """
-
-    state_id = ""
-    name = ""
+    # initialize class for file/db storage type
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship('Place', cascade='all, delete', backref='cities')
