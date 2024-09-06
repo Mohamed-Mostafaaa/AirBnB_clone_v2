b7d8683e4a502515a1ded81f283702e1bbbaecf2
Mohamed-Mostafaaa <momostafa110022@gmail.com>
2024-09-06 21:34:53 +0300

    amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""
-amenity module inheriting from the BaseModel
-"""
+"""This is the amenity class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
-from models.base_model import BaseModel
 
-
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
+    """This is the class for Amenity
+    Attributes:
+        name: input name
     """
-    Amenity Class
-    attributes:
-        name (str) -> Name of the amenity
-    """
-    name = ""
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+
+    # place_amenities = relationship('Place', secondary='place_amenity')
